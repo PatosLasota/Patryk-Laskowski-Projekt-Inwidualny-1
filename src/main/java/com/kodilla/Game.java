3f89347cb2d8f3ee943f74fb1f42c820bd29c8c2
Laskowski <patryk.laskowski.papug@gmail.com>
2025-11-24 15:29:24 +0100

    9.1 SoundManagerNotWorking

--- src/main/java/com/kodilla/Game.java
+++ src/main/java/com/kodilla/Game.java
@@ -24,20 +24,16 @@ public class Game {
     }
 
     public int checkWin() {
-        // Sprawdzanie wierszy, kolumn i przekątnych
         for (int i = 0; i < 3; i++) {
-            // Sprawdzanie wierszy
             if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
                 updateScore(board[i][0]);
                 return board[i][0] == 'O' ? 1 : 2;
             }
-            // Sprawdzanie kolumn
             if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
                 updateScore(board[0][i]);
                 return board[0][i] == 'O' ? 1 : 2;
             }
         }
-        // Sprawdzanie przekątnych
         if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
             updateScore(board[0][0]);
             return board[0][0] == 'O' ? 1 : 2;
---
+++ src/main/java/com/kodilla/SoundManager.java
@@ -0,0 +1,46 @@
+package com.kodilla;
+
+import javafx.scene.media.AudioClip;
+
+import java.awt.*;
+
+public class SoundManager {
+
+    private static final String CLICK_SOUND = "file:src/main/resources/click.mp3";
+    private static final String WIN_SOUND = "file:src/main/resources/win.mp3";
+    private static final String DRAW_SOUND = "file:src/main/resources/draw.mp3";
+
+    public static void playClickSound() {
+        try {
+            AudioClip sound = new AudioClip(CLICK_SOUND);
+            sound.play();
+        } catch (Exception e) {
+            System.out.println("Nie można odtworzyć dźwięku kliknięcia");
+        }
+    }
+
+    public static void playWinSound() {
+        try {
+            AudioClip sound = new AudioClip(WIN_SOUND);
+            sound.play();
+        } catch (Exception e) {
+            System.out.println("Nie można odtworzyć dźwięku wygranej");
+        }
+    }
+
+    public static void playDrawSound() {
+        try {
+            AudioClip sound = new AudioClip(DRAW_SOUND);
+            sound.play();
+        } catch (Exception e) {
+            System.out.println("Nie można odtworzyć dźwięku remisu");
+        }
+    }
+    public static void playBeep() {
+        try {
+            Toolkit.getDefaultToolkit().beep();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
